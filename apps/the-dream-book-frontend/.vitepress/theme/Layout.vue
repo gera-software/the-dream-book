413efddcc345cdb65afc7a061720031d603e3ad8
Gilmar Andrade <gilmar-andrade@outlook.com>
2023-12-26 11:51:17 -0300

    fix router links

--- apps/the-dream-book-frontend/.vitepress/theme/Layout.vue
+++ apps/the-dream-book-frontend/.vitepress/theme/Layout.vue
@@ -2,20 +2,25 @@
 import { useData } from 'vitepress'
 
 // https://vitepress.dev/reference/runtime-api#usedata
-const { site, frontmatter } = useData()
+const { site, frontmatter, page } = useData()
+
 </script>
 
 <template>
+      <pre>
+      {{ page }}
+      {{ site }}
+    </pre>
   <div v-if="frontmatter.home">
     <h1>{{ site.title }}</h1>
     <p>{{ site.description }}</p>
     <ul>
-      <li><a href="/markdown-examples.html">Markdown Examples</a></li>
-      <li><a href="/api-examples.html">API Examples</a></li>
+      <li><a href="./markdown-examples.html">Markdown Examples</a></li>
+      <li><a href="./api-examples.html">API Examples</a></li>
     </ul>
   </div>
   <div v-else>
-    <a href="/">Home</a>
+    <a href="./">Home</a>
     <Content />
   </div>
 </template>
