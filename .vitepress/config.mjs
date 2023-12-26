db34be1a38ae2a18a1d44d5f6b6d649b4dfc588b
Gilmar Andrade <gilmar-andrade@outlook.com>
2023-12-26 09:53:32 -0300

    created vitepress project

---
+++ .vitepress/config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "The Dream Book",
+  description: "Um livro que conta as histórias dos nossos melhores sonhos"
+})
