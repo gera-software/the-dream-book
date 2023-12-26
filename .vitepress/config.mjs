f87fe6d31f3d024023ccfc04a0a9e953f5a5ee42
Turbobot <turbobot@vercel.com>
2023-12-26 10:01:32 -0300

    feat(create-turbo): apply official-starter transform

--- .vitepress/config.mjs
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vitepress'
-
-// https://vitepress.dev/reference/site-config
-export default defineConfig({
-  title: "The Dream Book",
-  description: "Um livro que conta as histórias dos nossos melhores sonhos"
-})
